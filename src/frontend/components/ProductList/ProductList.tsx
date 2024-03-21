810b5afd704510350fb4f46dac017f36a7f1d02f
krisztian.klucsik <krisztian.klucsik@dynatrace.com>
2024-03-21 14:06:47 +0100

    slowload with flagd poc

--- src/frontend/components/ProductList/ProductList.tsx
+++ src/frontend/components/ProductList/ProductList.tsx
@@ -5,18 +5,22 @@ import { CypressFields } from '../../utils/Cypress';
 import { Product } from '../../protos/demo';
 import ProductCard from '../ProductCard';
 import * as S from './ProductList.styled';
+import { OpenFeatureProvider, OpenFeature } from '@openfeature/react-sdk';
 
 interface IProps {
   productList: Product[];
 }
 
 const ProductList = ({ productList }: IProps) => {
+  const client = OpenFeature.getClient();
   return (
-    <S.ProductList data-cy={CypressFields.ProductList}>
-      {productList.map(product => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </S.ProductList>
+    <OpenFeatureProvider client={client}>
+      <S.ProductList data-cy={CypressFields.ProductList}>
+        {productList.map(product => (
+            <ProductCard key={product.id} product={product} />
+        ))}
+      </S.ProductList>
+    </OpenFeatureProvider>
   );
 };
 
