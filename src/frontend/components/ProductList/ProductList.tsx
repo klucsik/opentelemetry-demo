8f1f570659d0ba050585024322c3970d29292fb0
krisztian.klucsik <krisztian.klucsik@dynatrace.com>
2024-03-26 11:22:47 +0100

    move OpenFeatureProvider to root level

--- src/frontend/components/ProductList/ProductList.tsx
+++ src/frontend/components/ProductList/ProductList.tsx
@@ -5,22 +5,18 @@ import { CypressFields } from '../../utils/Cypress';
 import { Product } from '../../protos/demo';
 import ProductCard from '../ProductCard';
 import * as S from './ProductList.styled';
-import { OpenFeatureProvider, OpenFeature } from '@openfeature/react-sdk';
 
 interface IProps {
   productList: Product[];
 }
 
 const ProductList = ({ productList }: IProps) => {
-  const client = OpenFeature.getClient();
   return (
-    <OpenFeatureProvider client={client}>
       <S.ProductList data-cy={CypressFields.ProductList}>
         {productList.map(product => (
             <ProductCard key={product.id} product={product} />
         ))}
       </S.ProductList>
-    </OpenFeatureProvider>
   );
 };
 
